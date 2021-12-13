315a39c383bf1dd110e1925c97540d75f9b94014
Mikhail Kulagin <prj115@hotmail.com>
2021-12-13 14:19:09 +0500

    Add project files.

---
+++ CopyFamily/FamilyMaker.cs
@@ -0,0 +1,106 @@
+﻿/*
+
+FamilyMaker
+
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Autodesk.Revit.DB;
+
+namespace CopyFamily
+{
+  internal class FamilyMaker
+  {
+    private const string FamilyTemplatePath = "C:\\ProgramData\\Autodesk\\RVT 2021\\Family Templates\\English\\Metric Generic Model.rft";
+    private Document doc;
+
+
+    public FamilyMaker(Autodesk.Revit.ApplicationServices.Application app)
+    {
+      doc = app.NewFamilyDocument(FamilyTemplatePath);
+    }
+
+
+    public void GrabGeometryOf(Document srcDoc)
+    {
+      var geomElems = new FilteredElementCollector(srcDoc).OfClass(typeof(GenericForm)).ToElements();
+      var elems = CollectLinked(geomElems);
+
+      if (elems.Any())
+      {
+        using (Transaction t = new Transaction(doc, "Copy elements from source"))
+        {
+          t.Start();
+
+          var cpo = new CopyPasteOptions();
+          cpo.SetDuplicateTypeNamesHandler(new DuplicateTypeNameHandlerUseDestinationTypes());
+          ElementTransformUtils.CopyElements(srcDoc, elems, doc, Transform.Identity, cpo);
+
+          t.Commit();
+        }
+      }
+    }
+
+
+    private static ICollection<ElementId> CollectLinked(IList<Element> elems)
+    {
+      HashSet<ElementId> dependencies = new HashSet<ElementId>();
+
+      foreach (var elem in elems)
+      {
+        CollectElementDependencies(elem, dependencies);
+      }
+
+      return dependencies;
+    }
+
+    private static void CollectElementDependencies(Element elem, HashSet<ElementId> dependencies)
+    {
+      if (dependencies.Contains(elem.Id))
+        return;
+
+      dependencies.Add(elem.Id);
+
+      foreach (var id in elem.GetDependentElements(new ElementOwnerViewFilter(ElementId.InvalidElementId)))
+      {
+        try
+        {
+          Element e = elem.Document.GetElement(id);
+
+          CollectElementDependencies(e, dependencies);
+
+          if (e is Dimension dimension)
+          {
+            ReferenceArray refs = dimension.References;
+            foreach (Reference reference in refs)
+              CollectElementDependencies(e.Document.GetElement(reference.ElementId), dependencies);
+          }
+        }
+        catch
+        { }
+      }
+    }
+
+
+    public void SaveFamily(string path)
+    {
+      var sao = new SaveAsOptions()
+      {
+        OverwriteExistingFile = true
+      };
+      doc.SaveAs(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "TestFam.rfa"), sao);
+    }
+  }
+
+
+  internal class DuplicateTypeNameHandlerUseDestinationTypes : IDuplicateTypeNamesHandler
+  {
+    public DuplicateTypeAction OnDuplicateTypeNamesFound(DuplicateTypeNamesHandlerArgs args)
+    {
+      return DuplicateTypeAction.UseDestinationTypes;
+    }
+  }
+}
---
+++ CopyFamily/RevitBinding.cs
@@ -0,0 +1,35 @@
+﻿/*
+
+Creates new family and copy geometry form current open family
+
+
+ */
+
+
+using System;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace CopyFamily
+{
+  [TransactionAttribute(TransactionMode.Manual)]
+  [RegenerationAttribute(RegenerationOption.Manual)]
+  public sealed class RevitBinding : IExternalCommand
+  {
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+    {
+      var uiApp = commandData.Application;
+
+      var maker = new FamilyMaker(uiApp.Application);
+      maker.GrabGeometryOf(uiApp.ActiveUIDocument.Document);
+
+      var path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "TestFam.rfa");
+      maker.SaveFamily(path);
+      uiApp.OpenAndActivateDocument(path);
+
+      return Result.Succeeded;
+    }
+  }
+}
